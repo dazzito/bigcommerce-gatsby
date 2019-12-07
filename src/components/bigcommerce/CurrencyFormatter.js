022804fac0cf16ac2f045fc0443ed89d16f3bd9e
dazzito <tl.leo90@hotmail.com>
2019-12-07 17:22:15 +0700

    Update CurrencyFormatter.js

--- src/components/bigcommerce/CurrencyFormatter.js
+++ src/components/bigcommerce/CurrencyFormatter.js
@@ -1,9 +1,9 @@
 const CurrencyFormatter = ({ currency, amount }) => {
-  const languageCode =
-    typeof window !== 'undefined'
-      ? window.navigator.language || 'en-US'
-      : 'en-US';
-  const formattedPrice = new Intl.NumberFormat(languageCode, {
+  // const languageCode =
+  //   typeof window !== 'undefined'
+  //     ? window.navigator.language || 'en-US'
+  //     : 'en-US';
+  const formattedPrice = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency
   }).format(amount);
